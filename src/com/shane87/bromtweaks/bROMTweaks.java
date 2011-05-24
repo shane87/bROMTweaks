46783fd4fc90a612fd22387b0c2265d9c6d51383
Eric Clothier <eric.clothier@yahoo.com>
2011-05-24 07:37:15 -0400

    first commit

---
+++ src/com/shane87/bromtweaks/bROMTweaks.java
@@ -0,0 +1,13 @@
+package com.shane87.bromtweaks;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class bROMTweaks extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
